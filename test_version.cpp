2cb77754f1a34b8fb99d6c8496ddd4994051b261
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-03-21 03:02:49 -0700

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -1,12 +1,11 @@
 #define BOOST_TEST_MODULE test_version
 
 #include <boost/test/unit_test.hpp>
-#include "lib.h"
 
-BOOST_AUTO_TEST_SUITE(test_version)
+#include "version.h"
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version() > 0);
+	BOOST_CHECK(PROJECT_VERSION_PATCH > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
