329ca563dbab4caa5460d55d8ada40f6c4b4cc35
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-03-21 02:40:57 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ip_filter/ip_filter.hpp>
+
+int main(int argc, char* argv[]){
+    if(argc > 2) {
+        std::cout << "Use ./deb_name <filepath_to_tsv/tsv_name.tsv>" << std::endl;
+    } else {
+        IpFilter ip_filter(argv[1]);
+        ip_filter.filter_ip_addresses();
+    }
+    return 0;
+}
