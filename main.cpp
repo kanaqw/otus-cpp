da21f42a429fc98938418906e7306141d7a7b2f5
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-18 19:27:42 +0300

    removed filepath

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "ip_filter/ip_filter.hpp"
 
-int main(int argc, char* argv[]){
-    if(argc > 2) {
-        std::cout << "Use ./deb_name <filepath_to_tsv/tsv_name.tsv>" << std::endl;
-    } else {
-        IpFilter ip_filter(argv[1]);
+int main(){
+        IpFilter ip_filter();
         ip_filter.filter_ip_addresses();
-    }
-    return 0;
+        return 0;
 }
