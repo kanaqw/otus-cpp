97dd832dff9a2ab2cc7b7f31ca08896889eeca30
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-18 20:57:37 +0300

    Create shapes.hpp

---
+++ editor/include/shapes.hpp
@@ -0,0 +1,22 @@
+#include "common.hpp"
+
+class Shape {
+    public:
+        virtual ~Shape() = default;
+        virtual void draw() const = 0;
+        void set_index(int index) {
+            this->index = index;
+        }
+        int get_index() const {
+            return index;
+        }
+    private:
+        int index{};
+
+};
+
+class Circle : public Shape {
+        void draw() const override {
+            std::cout << "Drawing a circle" << std::endl;
+        }
+};
