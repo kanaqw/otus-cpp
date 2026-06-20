ba7384f391865e057a34a0ec8bf00af2a7532db0
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-20 16:44:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
-#include <iostream>
-#include "ip_filter/ip_filter.hpp"
+#include "include/matrix.hpp"
 
-int main(int argc, char* argv[]){
-    if(argc > 2) {
-        std::cout << "Use ./deb_name <filepath_to_tsv/tsv_name.tsv>" << std::endl;
-    } else {
-        IpFilter ip_filter(argv[1]);
-        ip_filter.filter_ip_addresses();
+int main() {
+    Matrix<int, 0> matrix;
+    
+    //main diagonal
+    for (int i = 0; i < 10; i++){
+        matrix[i][i] = i;
     }
+    //secondary diagonal
+    for (int i = 0; i < 10; i++){
+        matrix[i][9-i] = 9-i;
+    }
+
+    //fragment output
+    for (int row = 1; row <= 8; ++row){
+        for (int col = 1; col <= 8; ++col){
+            std::cout << matrix[row][col];
+            if (col < 8) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    //number of occupied cells
+    std::cout << "Occupied cells: " << matrix.size() << std::endl;
+
+    //all stored cells
+    std::cout << "Stored cells:" << std::endl;
+    for (const auto& item : matrix) {
+        int x = item.first.first;
+        int y = item.first.second;
+        int value = item.second;
+
+        std::cout << "[" << x << "][" << y << "] = " << value << std::endl;
+
+    }
+
     return 0;
 }
