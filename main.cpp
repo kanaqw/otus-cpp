7decde3b3c8377828c179c36f971a3efe8c27f58
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-21 21:28:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-#include "ip_filter/ip_filter.hpp"
+#include "parser/parser.hpp"
 
-int main(int argc, char* argv[]){
-    if(argc > 2) {
-        std::cout << "Use ./deb_name <filepath_to_tsv/tsv_name.tsv>" << std::endl;
-    } else {
-        IpFilter ip_filter(argv[1]);
-        ip_filter.filter_ip_addresses();
+int main(int argc, char* argv[]) {
+    size_t default_size = 3;
+    if (argc > 1){
+        try {
+            default_size = std::stoul(argv[1]);
+        } catch (...) {
+            std::cerr << "Invalid argument, using default size - 3\n";
+        }
     }
+
+    parser::PackHandler handler(default_size);
+    auto console_logger = std::make_shared<parser::ConsoleLogger>();
+    auto file_logger = std::make_shared<parser::FileLogger>();
+
+    handler.subscribe(console_logger);
+    handler.subscribe(file_logger);
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+        handler.add_cmd_to_pack(line);
+    }
+
+    handler.flush_eof();
+
     return 0;
 }
