a61c4ea7b724250e89b34e853170fd73e23b00a4
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-21 21:40:48 +0300

    Update parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -23,41 +23,13 @@ namespace parser {
     class ConsoleLogger : public IOListener {
         public:
         void update(const std::vector<std::string>& block,
-                            time_t time) override {
-            if (!block.empty()){
-                std::cout << "Bulk: " ;
-                for (auto it = block.begin();const auto& cmd : block){
-                    std::cout << cmd;
-                    if (it!=block.end()){
-                        std::cout << ",";
-                    }
-                }
-                std::cout << "\n";
-            }
-
-        }
+                            time_t time) override;
     };
 
     class FileLogger : public IOListener {
         public:
         void update(const std::vector<std::string>& block,
-                            time_t time) override {
-            if (!block.empty()){
-                std::string filename = "Bulk" + std::to_string(time) + ".log";
-                std::ofstream file(filename);
-
-                if (file.is_open()) {
-                    file << "bulk: ";
-                    for (auto it = block.begin();const auto& cmd : block){
-                        std::cout << cmd;
-                        if (it!=block.end()){
-                            std::cout << ",";
-                        }
-                    }
-                    std::cout << "\n";
-                }
-            }
-        }
+                            time_t time) override ;
     };
 
     class PackHandler {
@@ -69,67 +41,17 @@ namespace parser {
           timestamp_(0)
           {}
 
-        void subscribe(std::shared_ptr<IOListener> listener) {
+        inline void subscribe(std::shared_ptr<IOListener> listener) {
             listeners_.push_back(listener);
         }
-
-        void add_cmd_to_pack(const std::string& cmd){
-            if (cmd == "{") {
-                handle_open_bracket();
-            } else if (cmd == "}") {
-                handle_close_bracket();
-            } else {
-                handle_regular_cmd(cmd);
-            }
-        }
-
-        void flush_eof() {
-            if (currentState_ == CmdType::STATIC && !commands_.empty()){
-                notify();
-            }
-            commands_.clear();
-        }
+        void add_cmd_to_pack(const std::string& cmd);
+        void flush_eof();
 
         private:
-            void handle_open_bracket(){
-                if (currentState_ == CmdType::STATIC) {
-                    if (!commands_.empty()) {
-                        notify();
-                    }
-                    currentState_ = CmdType::DYNAMIC;
-                }
-                brackets_.push('{');
-            }
-
-            void handle_close_bracket(){
-                if (currentState_ == CmdType::DYNAMIC){
-                    if (!brackets_.empty()){
-                        brackets_.pop();
-                    }
-                    if (brackets_.empty()){
-                        notify();
-                        currentState_ = CmdType::STATIC;
-                    }
-                }
-            }
-
-            void handle_regular_cmd(const std::string& cmd){
-                if (commands_.empty()){
-                    timestamp_ = std::time(nullptr);
-                }
-                commands_.push_back(cmd);
-                if (currentState_ == CmdType::STATIC &&
-                    commands_.size() == packSize_){
-                        notify();
-                    }
-            }
-
-            void notify() {
-                for (auto& listener : listeners_) {
-                    listener->update(commands_, timestamp_);
-                }
-                commands_.clear();
-            }
+            void handle_open_bracket();
+            void handle_close_bracket();
+            void handle_regular_cmd(const std::string& cmd);
+            void notify(); 
 
             std::vector<std::string> commands_;
             std::vector<std::shared_ptr<IOListener>> listeners_;
