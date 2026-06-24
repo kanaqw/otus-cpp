feca348ed2ae48037be1f846d1fef7175dea334c
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-24 19:18:30 +0300

    Update main.cpp

--- ip_filter/src/main.cpp
+++ ip_filter/src/main.cpp
@@ -3,6 +3,8 @@
 
 int main(){
         IpFilter ip_filter;
+        std::cout << "Enter string of ip's in next sequence: text1 \\t text2 \\t text3" << std::endl;
+        std::cout << "type empty line to finish" << std::endl
         ip_filter.filter_ip_addresses();
         return 0;
 }
