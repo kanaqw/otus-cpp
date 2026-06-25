c3083e57a5c97df38fa03c4c99ffdc998dc82501
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-06-25 11:02:12 +0300

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -24,7 +24,7 @@ namespace parser {
         void SetUp() override {
             handler = std::make_unique<PackHandler>(default_pack_size);
             spy_listener = std::make_shared<TestListener>();
-            handler->subscrube(spy_listener); 
+            handler->subscribe(spy_listener); 
         }
     
        
