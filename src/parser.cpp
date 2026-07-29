f31103fdfe841d7aa038f2eb6a3bb15a9eb2f82a
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-07-29 23:10:28 +0300

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,38 +1,84 @@
 #include "parser.hpp"
+
 namespace parser {
 
-void ConsoleLogger::update(const std::vector<std::string>& block,
-                            [[maybe_unused]] time_t time) {
-     if (!block.empty()){
-          std::cout << "Bulk: ";
-          for (auto it = block.begin();const auto& cmd : block){
-              std::cout << cmd;
-              if (it!=block.end()){
-                  std::cout << ",";
-              }
-          }
-          std::cout << "\n";
+namespace {
+    std::atomic<size_t> g_file_seq{0};
+}
+
+ConsoleLogger::ConsoleLogger()
+    : thread_(&ConsoleLogger::worker_loop, this) {}
+
+ConsoleLogger::~ConsoleLogger() {
+    queue_.stop();
+    if (thread_.joinable()) {
+        thread_.join();
     }
-};
+}
+
+void ConsoleLogger::update(const std::vector<std::string>& block,
+                            time_t time) {
+    queue_.push(Bulk{block, time});
+}
+
+void ConsoleLogger::worker_loop() {
+    while (auto bulk = queue_.pop()) {
+        if (bulk->commands.empty()) {
+            continue;
+        }
+        std::cout << "Bulk: ";
+        for (auto it = bulk->commands.begin(); const auto& cmd : bulk->commands){
+            std::cout << cmd;
+            if (++it != bulk->commands.end()){
+                std::cout << ",";
+            }
+        }
+        std::cout << "\n";
+    }
+}
+
+FileLogger::FileLogger()
+    : file_1_(&FileLogger::worker_loop, this, std::ref(queue1_)),
+      file_2_(&FileLogger::worker_loop, this, std::ref(queue2_)) {}
+
+FileLogger::~FileLogger() {
+    queue1_.stop();
+    queue2_.stop();
+    if (file_1_.joinable()) {
+        file_1_.join();
+    }
+    if (file_2_.joinable()) {
+        file_2_.join();
+    }
+}
 
 void FileLogger::update(const std::vector<std::string>& block,
                             time_t time) {
-      if (!block.empty()){
-          std::string filename = "Bulk" + std::to_string(time) + ".log";
-          std::ofstream file(filename);
+    size_t idx = counter_.fetch_add(1, std::memory_order_relaxed) % 2;
+    (idx == 0 ? queue1_ : queue2_).push(Bulk{block, time});
+}
 
-          if (file.is_open()) {
-              file << "bulk: ";
-              for (auto it = block.begin();const auto& cmd : block){
-                  std::cout << cmd;
-                  if (it!=block.end()){
-                      std::cout << ",";
-                  }
-              }
-              std::cout << "\n";
-          }
-      }
-};
+void FileLogger::worker_loop(SafeQueue<Bulk>& queue) {
+    while (auto bulk = queue.pop()) {
+        if (bulk->commands.empty()) {
+            continue;
+        }
+        size_t seq = g_file_seq.fetch_add(1, std::memory_order_relaxed);
+        std::string filename = "Bulk" + std::to_string(bulk->time) + "_" + std::to_string(seq) + ".log";
+        std::ofstream file(filename);
+
+        if (file.is_open()) {
+            file << "bulk: ";
+            for (auto it = bulk->commands.begin(); const auto& cmd : bulk->commands){
+                file << cmd;
+                if (++it != bulk->commands.end()){
+                    file << ",";
+                }
+            }
+            file << "\n";
+        }
+    }
+}
 
 void PackHandler::add_cmd_to_pack(const std::string& cmd){
    if (cmd == "{") {
