88000c0d7eaaf3f7bd41df901b02980fd2768bc8
kanaqw <81157913+kanaqw@users.noreply.github.com>
2026-08-04 18:49:47 +0300

    Update and rename parser.cpp to mapper.cpp

---
+++ src/mapper.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include "common.hpp"
+
+int main() {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (auto price = extract_price(line)) {
+            std::cout << "price\t" << *price << "\n";
+        }
+    }
+    return 0;
+}
